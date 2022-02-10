d8fdbf31fe8c941c4b48966ed43d6365950e712d
DCBrady <99332506+DCBrady@users.noreply.github.com>
2022-02-10 13:17:02 +1100

    Delete js directory

--- js/assignment1javascript.js
+++
@@ -1,7 +0,0 @@
-﻿//Adding images to sections
-//document.getElementById("technologies").style.backgroundImage = "url('~/images/Homepage/ai.jpg')";
-
-//var sections = document.getElementsByClassName("technologies");
-//for (var i = 0; i < sections.length; i++) {
-//    sections[i].style.backgroundImage = "url(~/images/Homepage/ai.jpg"
-//}
